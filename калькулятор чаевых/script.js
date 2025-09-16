9f3e611c558e8d5a4e3c22b97ee8f25d5abe345f
yzaru <yzarrubin@yandex.ru>
2025-09-17 02:14:39 +0300

    Add files via upload

---
+++ калькулятор чаевых/script.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    const summ = document.querySelector('.oneinp');
+    const countButton = document.querySelector('.count');
+    const quality = document.querySelector('select');
+    const amount = document.querySelector('.twoinp');
+
+    const result1 = document.querySelector('resultone');
+    const resultall = document.querySelector('.resulttwo');
+
+    actionButton.addEventListener('click', () => {
+        resultone.innerHTML = oneinp.value
+    });
+};
